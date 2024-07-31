904023121d03809810dddff6abc944a044dd8fd3
Muthu-Ganapathi <147491315+MuthuG-Touchmarkdes@users.noreply.github.com>
2024-08-01 00:05:09 +0530

    landing screen

--- src/Pages/Auth/landing.js
+++ src/Pages/Auth/landing.js
@@ -5,8 +5,15 @@ function Landing(params) {
     return (
         <div className="w-full">
        <ContentHeader />
-       <div>
-        <img src={Logo} />
+       <div className="position-absolute w-100">
+       <div className="text-center position-absolute w-100" >
+        <img src={Logo} height={100} />
+       </div>
+       <div className="text-end position-absolute w-100 align-content-center" style={{height:"100px"}}>
+       <div className="button-style px-4 py-2">
+  <div className="button-style1">LOG IN</div>
+</div>
+       </div>
        </div>
        </div>
     );
