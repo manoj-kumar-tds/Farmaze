7da0d0b1d9281900e697366be5899125484100b6
Muthu-Ganapathi <147491315+MuthuG-Touchmarkdes@users.noreply.github.com>
2024-08-01 09:14:45 +0530

    design

--- src/Pages/Auth/landing.js
+++ src/Pages/Auth/landing.js
@@ -1,19 +1,23 @@
 import React from "react";
 import {ContentHeader} from "../../Components/header";
 import Logo from "../../assets/logo_header.png";
+import LandingLogo from "../../assets/landing_bg.png"
 function Landing(params) {
     return (
-        <div className="w-full">
+        <div className="w-ful">
        <ContentHeader />
-       <div className="position-absolute w-100">
+       {/* <div className="login-header"> */}
+       <div className="position-absolute w-100 bg-red login-header py-3">
        <div className="text-center position-absolute w-100" >
-        <img src={Logo} height={100} />
+        <img src={Logo} height={80} />
        </div>
-       <div className="text-end position-absolute w-100 align-content-center" style={{height:"100px"}}>
-       <div className="button-style px-4 py-2">
-  <div className="button-style1">LOG IN</div>
-</div>
+       <div className="text-end position-absolute w-100 align-content-center" style={{height:"80px"}}>
+        <button className="button-style px-3 py-2">LOG IN</button>
+        </div>
        </div>
+       <div className=" relative flex-grow flex flex-col">
+       <img className="w-100 content" src={LandingLogo} />
+       {/* </div> */}
        </div>
        </div>
     );
